5a60755050908bd8790aba7cade2367a1446f531
karl18 <karl18@scsxnet.uk>
2021-06-30 17:20:31 +0100

    update app

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:8
-ADD target/HelloWorldRESTAPI.jar HelloWorldRESTAPI.jar
-EXPOSE 9021
-ENTRYPOINT ["java", "-jar", "HelloWorldRESTAPI.jar"]
--- src/main/java/com/apitest/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/apitest/helloworld/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.apitest.helloworld.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.context.properties.*;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.*;
@@ -7,48 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/v1")
 @ConfigurationProperties(prefix="config")
 @Component
-public class HelloWorldController {
+@Slf4j
+public class HelloWorldController {	
 
-	private String text;
-	private String configMap;
-	private String configSecret;
-	
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	} 
-	
-	public String getConfigMap() {
-		return configMap;
-	}
-
-	public void setConfigMap(String configMap) {
-		this.configMap = configMap;
-	} 
-	
-	public String getConfigSecret() {
-		return configSecret;
-	}
-
-	public void setConfigSecret(String configSecret) {
-		this.configSecret = configSecret;
-	}  
+	private static final Logger LOGGER = LogManager.getLogger();	
 
 	@RequestMapping(value = "/helloworld", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String HelloWorld() {  
-		
 
-		return "{\"Text\": " + getText() + 
-		",\"Config Map\": \"" + getConfigMap() + 
-		"\",\"ConfigSecret\": \"" + getConfigSecret() + 
-		"\",\"Non config value\": \"" + "HardCodedValue" + 
-		"\"}";  	  
+		LOGGER.info("Some logged information");		
+
+		return "Spring API 2 - Hello Galaxy";  	  
 	}	
 }
